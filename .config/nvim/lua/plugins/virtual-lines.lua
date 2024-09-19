3a1db15ad8a86b9955aab0874d6d3ac658ef5e3e
Gabriel Baims <gbaims@users.noreply.github.com>
2024-09-19 19:24:35 -0300

    improve lines in lazyvim

---
+++ .config/nvim/lua/plugins/virtual-lines.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = { indent = { char = "▏" } },
+  },
+  {
+    "lukas-reineke/virt-column.nvim",
+    opts = { char = { "▏" }, virtcolumn = "100", highlight = { "IblIndent" } },
+  },
+}
