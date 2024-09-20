af6f6e0dcce9c0a68bc73677babdf5861d797d66
Gabriel Baims <gbaims@users.noreply.github.com>
2024-09-19 23:53:51 -0300

    add borders to neovim ui popups

---
+++ .config/nvim/lua/plugins/borders.lua
@@ -0,0 +1,15 @@
+return {
+  { "williamboman/mason.nvim", opts = { ui = { border = "rounded" } } },
+  { "folke/noice.nvim", opts = { presets = { lsp_doc_border = true } } },
+  { "neovim/nvim-lspconfig", opts = { diagnostics = { float = { border = "rounded" } } } },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local window = {
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+      }
+      opts.window = { completion = window, documentation = window }
+    end,
+  },
+}
